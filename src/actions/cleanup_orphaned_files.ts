62fe8c2debc6fe140a40cc7826734df5dda6d34a
Santiago Lezica <salezica@gmail.com>
2026-03-19 18:31:50 -0300

    library: cleanup service file, forgotten :D

---
+++ src/actions/cleanup_orphaned_files.ts
@@ -0,0 +1,31 @@
+import type { DatabaseService, FileStorageService } from '../services'
+import type { Action, ActionFactory } from '../store/types'
+import { CLIPS_DIR } from './constants'
+
+export interface CleanupOrphanedFilesDeps {
+  db: DatabaseService
+  files: FileStorageService
+}
+
+export type CleanupOrphanedFiles = Action<[]>
+
+export const createCleanupOrphanedFiles: ActionFactory<CleanupOrphanedFilesDeps, CleanupOrphanedFiles> = (deps) => (
+  async () => {
+    const { db, files } = deps
+
+    const knownUris = db.getAllFileUris()
+
+    const bookFiles = await files.listFiles(files.audioDirectoryPath)
+    const clipFiles = await files.listFiles(CLIPS_DIR)
+
+    const orphans = [...bookFiles, ...clipFiles].filter(uri => !knownUris.has(uri))
+
+    for (const uri of orphans) {
+      await files.deleteFile(uri).catch(() => {})
+    }
+
+    if (orphans.length > 0) {
+      console.log(`[Cleanup] Deleted ${orphans.length} orphaned file(s)`)
+    }
+  }
+)
