e47f0dc2bbacf4cdb26aa6c0d5adb028c4b695de
Santiago Lezica <salezica@gmail.com>
2026-03-22 23:00:32 -0300

    lint: remove custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,9 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
-const ivyPlugin = require('./eslint');
-
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
-  },
-  {
-    plugins: {
-      ivy: ivyPlugin,
-    },
-    rules: {
-      'ivy/jsx-align-ternary-chain': 'error',
-      'ivy/jsx-align-ternary-single': 'error',
-      'ivy/jsx-newline-around-multiline': 'warn',
-    },
   },
 ]);
