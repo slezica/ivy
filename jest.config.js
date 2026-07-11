97a0540ecc4d21cf1455f5dee181fd55b7fd4065
Santiago Lezica <salezica@gmail.com>
2026-07-11 16:38:28 +0000

    qa: transform zustand and immer in jest

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@shopify/react-native-skia|react-native-fs|react-native-track-player|@react-native-google-signin/google-signin|whisper.rn)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@shopify/react-native-skia|react-native-fs|react-native-track-player|@react-native-google-signin/google-signin|whisper.rn|zustand|immer)',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
